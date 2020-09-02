c939ae6c311b5d8b04ae292ffde93958210153cb
Marc David <zubben@gmail.com>
2020-09-02 14:31:04 +0100

    prettier formatting

--- commands/owner/reload.js
+++ commands/owner/reload.js
@@ -1,31 +1,27 @@
 export default {
-  name: "reload",
-  description: "Reloads a command",
+  name: 'reload',
+  description: 'Reloads a command',
   execute(message, args) {
-    const commandName = args[0].toLowerCase();
+    const commandName = args[0].toLowerCase()
     const command =
       message.client.commands.get(commandName) ||
-      message.client.commands.find(
-        (cmd) => cmd.aliases && cmd.aliases.includes(commandName)
-      );
+      message.client.commands.find((cmd) => cmd.aliases && cmd.aliases.includes(commandName))
+    if (message.guild.ownerID !== message.author.id)
+      return message.channel.send(`You must be the owner of this server.`)
 
     if (!command) {
-      return message.channel.send(
-        `There is no command with name or alias \`${commandName}\`, ${message.author}!`
-      );
+      return message.channel.send(`There is no command with name or alias \`${commandName}\`, ${message.author}!`)
     }
 
-    delete require.cache[require.resolve(`./${command.name}.js`)];
+    delete require.cache[require.resolve(`./${command.name}.js`)]
 
     try {
-      const newCommand = require(`./${command.name}.js`);
-      message.client.commands.set(newCommand.name, newCommand);
-      message.channel.send(`Command \`${command.name}\` was reloaded!`);
+      const newCommand = require(`./${command.name}.js`)
+      message.client.commands.set(newCommand.name, newCommand)
+      message.channel.send(`Command \`${command.name}\` was reloaded!`)
     } catch (error) {
-      console.log(error);
-      message.channel.send(
-        `There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``
-      );
+      console.log(error)
+      message.channel.send(`There was an error while reloading a command \`${command.name}\`:\n\`${error.message}\``)
     }
   },
-};
+}
